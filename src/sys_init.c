7beedaf9132a797c350f22acaed559d2d8dfd037
HammerPiano <noamw2000@gmail.com>
2021-01-30 19:04:22 +0200

    Added init code for globals in ADC. Maybe there is something wrong when compiling?

--- src/sys_init.c
+++ src/sys_init.c
@@ -7,6 +7,7 @@ Purpose: Initialize the basic functionality of the chip, including the clock,
 usb debug msgs and watchdogs
 */
 
+#include "ADC.h"
 #include "DMA.h"
 #include "GPIO.h"
 #include "RCC.h"
@@ -16,4 +17,5 @@ void chip_init()
 	RCC_init_clock();
 	GPIO_startup();
 	DMA_startup();
+	ADC_startup();
 }
