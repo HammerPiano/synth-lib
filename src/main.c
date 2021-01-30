7a37699533b413e22a6438555ec051830606b0c0
HammerPiano <noamw2000@gmail.com>
2021-01-30 19:04:42 +0200

    Added ADC, DMA, gpio test code

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "ADC.h"
 #include "DMA.h"
 #include "GPIO.h"
 
@@ -19,38 +20,29 @@ void delay(uint32_t length)
 The segments are marked from the top, clockwise a-f and center is g
 a to A5, B to A6 etc.
 Digits is common anode
+									 0	   1	 2	   3	 4	   5	 6	   7	 8	  9		A	  B		C     D     E
 */
-const uint16_t LED_7SEG_VALUES[] = { 0x800, 0xf20, 0x480, 0x600, 0x320, 0x240, 0x40, 0xf00, 0x0, 0x200 };
+const uint16_t LED_7SEG_VALUES[] = { 0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0x78, 0x0, 0x10, 0x08, 0x03, 0x46, 0x21, 0x06, 0x0E };
 
 int main()
 {
-
-	/*uint8_t			 i		= 0;
+	uint16_t		 temp = 0, pins[] = { 8 };
 	GPIO_PIN_ARRAY_t led	= { 0 };
 	GPIO_PIN_ARRAY_t button = { 0 };
-	GPIO_array_init(&button, GPIO_PORT_B, 9, 9, GPIO_MODE_INPUT, GPIO_CONFIG_INPUT_PULL_UP);
-	GPIO_array_init(&led, GPIO_PORT_A, 5, 11, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_OPEN_DRAIN);
+	GPIO_array_init(&button, GPIO_PORT_B, 0, 0, GPIO_MODE_INPUT, GPIO_CONFIG_INPUT_ANALOG);
+	GPIO_array_init(&led, GPIO_PORT_A, 0, 6, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_OPEN_DRAIN);
+	ADC_init(pins, 1, &temp);
+
 	// Clear display
-	GPIO_array_write_value(&led, 0xFE0);
+	GPIO_array_write_value(&led, 0x7F);
 	while (1)
 	{
 		// each button press increase the index one time only
-		while (GPIO_array_read_all(&button) != 0)
-		{
-			delay(1000);
-		}
-		GPIO_array_write_value(&led, LED_7SEG_VALUES[i++]);
-		if (i >= 10)
-		{
-			i = 0;
-		}
+		ADC_start(ADC_mode_single, 1);
+		delay(1000);
+		DMA_stop_channel(DMA_CH1_ADC1);
+		temp /= 256;
+		GPIO_array_write_value(&led, LED_7SEG_VALUES[temp]);
 		// wait for release
-		while (GPIO_array_read_all(&button) == 0)
-		{
-			delay(1000);
-		}
-	}*/
-	DMA_address_t periph = { .address = 0x40010800, .access_size = DMA_ACCESS_32BIT, .increament_address = false };
-	DMA_address_t memory = { .address = 0x200000F0, .access_size = DMA_ACCESS_32BIT, .increament_address = false };
-	DMA_init_channel(DMA_CH1, &periph, &memory, DMA_CH_PRIORITY_HIGH, DMA_DIRECTION_MEM_TO_PERIPH, 0);
+	}
 }
