9bb4ba77c5e4614ecce8b3fde709426e3b1585b2
HammerPiano <noamw2000@gmail.com>
2021-04-16 16:03:35 +0300

    Added main code as an example

--- src/main.c
+++ src/main.c
@@ -9,6 +9,21 @@
 #define LED_ON	(true)
 #define LED_OFF (false)
 
+#define LED_COUNT (7)
+#define LED_X_START (0)
+#define LED_X_END (LED_X_START + LED_COUNT - 1)
+#define LED_X_PORT (GPIO_PORT_A)
+
+#define LED_Y_START (5)
+#define LED_Y_END (LED_Y_START + LED_COUNT - 1)
+#define LED_Y_PORT (GPIO_PORT_B)
+// 12-bit ADC
+#define ADC_MIDDLE (2048)
+// Seperate the 0-4095 range to 7 parts, we need 6 thresholds (TH)
+#define ADC_LED_RANGE (4096 / LED_COUNT)
+
+
+
 void delay(uint32_t ms)
 {
 	for (uint32_t i = 0; i < ms; i++)
@@ -30,55 +45,26 @@ const uint16_t LED_7SEG_VALUES[] = { 0x40, 0x79, 0x24, 0x30, 0x19, 0x12, 0x02, 0
 
 int main()
 {
-	const uint8_t	 adc_pins = {8,9};// pins B0, B1 are inputs 8,9 of the ADC1
+	uint8_t	 adc_pins[] = {8, 9};// pins B0, B1 are inputs 8,9 of the ADC1
 	uint16_t 		 adc_data[2] = {0}; // X and Y values of potentiometer
 	GPIO_PIN_ARRAY_t x_bargraph	= { 0 };
 	GPIO_PIN_ARRAY_t y_bargraph	= { 0 };
-	GPIO_PIN_ARRAY_t err_led  = { 0 };
-	GPIO_array_init(&x_bargraph, GPIO_PORT_A, 0, 6, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_PUSH_PULL);
-
-	GPIO_array_init(&err_led, GPIO_PORT_C, 13, 13, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_OPEN_DRAIN);
-	// ADC_init(adc_pins, sizeof(adc_pins) / sizeof(adc_pins[0]), adc_data);
-	if (GPIO_array_init(&y_bargraph, GPIO_PORT_B, 5, 11, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_PUSH_PULL) == GPIO_NO_ERR)
-	{
-		GPIO_array_write_value(&err_led, 1);
-	}
-	else
-	{
-		GPIO_array_write_value(&err_led, 1);
-	}
-
-	GPIO_array_write_all(&y_bargraph, 1);
-	GPIO_array_write_all(&x_bargraph, 1);
-	delay(100);
+	GPIO_PIN_ARRAY_t adc_inputs = { 0 };
+	GPIO_array_init(&x_bargraph, LED_X_PORT, LED_X_START, LED_X_END, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_PUSH_PULL);
+	GPIO_array_init(&y_bargraph, LED_Y_PORT, LED_Y_START, LED_Y_END, GPIO_MODE_OUTPUT, GPIO_CONFIG_OUTPUT_PUSH_PULL);
+	GPIO_array_init(&adc_inputs, GPIO_PORT_B, 0, 1, GPIO_MODE_INPUT, GPIO_CONFIG_INPUT_ANALOG);
+	ADC_init(adc_pins, sizeof(adc_pins) / sizeof(adc_pins[0]), adc_data);
 	GPIO_array_write_all(&y_bargraph, 0);
 	GPIO_array_write_all(&x_bargraph, 0);
 	while (1)
 	{
-		for (size_t i = 0; i < 7; i++)
-		{
-			GPIO_array_write_value(&x_bargraph, 1 << i);
-			delay(10);
-		}
-		GPIO_array_write_all(&x_bargraph, 0);
-		for (size_t i = 0; i < 7; i++)
-		{
-			GPIO_array_write_value(&y_bargraph, 1 << i);
-			delay(10);
-		}
-		GPIO_array_write_all(&y_bargraph, 0);
-
-		/*
 		// each button press increase the index one time only
-		ADC_start(ADC_mode_single, 1);
-		while(ADC_get_flag(ADC_end_of_conversion) == 0)
-		{
-			asm ("nop");
-		}
+		ADC_start(ADC_mode_single, 2);
+		delay(1);
 		DMA_stop_channel(DMA_CH1_ADC1);
-		temp /= 256;
-		GPIO_array_write_value(&x_bargraph, LED_7SEG_VALUES[temp]);
+		
+		GPIO_array_write_value(&x_bargraph, 1 << (adc_data[0] / ADC_LED_RANGE));
+		GPIO_array_write_value(&y_bargraph, 1 << (adc_data[1] / ADC_LED_RANGE));
 		// wait for release
-		*/
 	}
 }
